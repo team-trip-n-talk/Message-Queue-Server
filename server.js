0b17656ce3c803bd7763ab70941dc13fabb8f4c3
Tia Rose <shortyshort789@aol.com>
2019-06-24 15:06:07 -0700

    working on getting and receiving messages

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 'use strict';
 
-require('dotenv').config();
+// require('dotenv').config();
 const Q = require('@nmq/q/server');
 
 Q.start();
 
 const api = new Q('api');
-api.monitorEvent('sendMessage');
+api.monitorEvent('msg');
