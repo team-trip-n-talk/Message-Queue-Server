6e5e3103911c88659faec99f4273ec7f5581faab
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-25 09:37:25 -0700

    last cleanup

--- server.js
+++ server.js
@@ -1,16 +1,5 @@
-// 'use strict';
-
-// require('dotenv').config();
-// const Q = require('@nmq/q/server');
-
-// Q.start();
-
-// const messageQ = new Q('messageQ');
-// messageQ.monitorEvent('message');
-
 'use strict';
 
-// const app = require('express').createServer();
 require('dotenv').config();
 
 const io = require('socket.io')(process.env.PORT);
@@ -21,10 +10,3 @@ io.on('connection', socket => {
     socket.broadcast.emit('message', payload);
   });
 });
-
-// io.on('connection', socket => {
-//   console.log(`Connected socket id: ${socket.id}`);
-//   socket.on('file-error', payload => {
-//     socket.broadcast.emit('file-error', payload);
-//   });
-// });
