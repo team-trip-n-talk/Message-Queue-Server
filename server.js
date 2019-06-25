fa678b1b79340967c6e011ba730a54e7bf2b1547
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-25 08:41:31 -0700

    socket io work

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+// 'use strict';
+
+// require('dotenv').config();
+// const Q = require('@nmq/q/server');
+
+// Q.start();
+
+// const messageQ = new Q('messageQ');
+// messageQ.monitorEvent('message');
+
 'use strict';
 
-require('dotenv').config();
-const Q = require('@nmq/q/server');
+const io = require('socket.io')(process.env.PORT);
 
-Q.start();
+io.on('connection', socket => {
+  console.log(`Connected socket id: ${socket.id}`);
+  socket.on('message', payload => {
+    socket.broadcast.emit('message', payload);
+  });
+});
 
-const messageQ = new Q('messageQ');
-messageQ.monitorEvent('message');
+// io.on('connection', socket => {
+//   console.log(`Connected socket id: ${socket.id}`);
+//   socket.on('file-error', payload => {
+//     socket.broadcast.emit('file-error', payload);
+//   });
+// });
