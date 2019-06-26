d6b9d08c98ec0b09430c6c405f0f7491dd5fcf32
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-26 15:19:12 -0700

    adding express and jsdocs

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ require('dotenv').config();
 const io = require('socket.io')(process.env.PORT);
 const jwt = require('jsonwebtoken');
 
+const express = require('express');
+
+// Prepare the express app
+const app = express();
+
+//Jsdocs
+app.use(express.static('docs'));
+app.use('/docs', express.static('docs'));
+
 
 io.use(function(socket, next){
   if (socket.handshake.query && socket.handshake.query.token){
